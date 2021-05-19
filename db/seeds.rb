e16e2e53720884aec24e1bbaf8ee190d8f06f4f2
leandrohorizon <leandro.mcontato@gmail.com>
2021-05-19 16:10:19 -0300

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!({ description: 'Bitcoin', acronym: 'BTC',
+               url_image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png' })
+Coin.create!({ description: 'Etherium', acronym: 'ETH',
+               url_image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png' })
+
+# rails -T db
