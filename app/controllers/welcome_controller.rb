aee785acd0c68d2a654b3c17bd39d2652119628e
leandrohorizon <leandro.mcontato@gmail.com>
2021-10-18 12:06:16 -0300

    Uso de cookies no controller welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @my_name = my_name
+  end
+end
+
+def my_name
+  cookies[:my_name] ||= 'leandro'
+  # session[:my_name] ||= 'leandro'
 end
